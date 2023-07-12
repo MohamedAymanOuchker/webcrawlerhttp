589c07654ec3b3cdcfc7c890312e966b7bd0c4fa
Mohamed Ayman Ouchker <50711522+MohamedAymanOuchker@users.noreply.github.com>
2023-07-12 11:36:22 +0100

    add downloadReport & convertToExcel functions

--- report.js
+++ report.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const XLSX = require('xlsx');
+
 function printReport(pages) {
     console.log("===========")
     console.log("REPORT")
@@ -11,6 +14,9 @@ function printReport(pages) {
     console.log("===========")
     console.log("END REPORT")
     console.log("===========")
+    
+    const reportData = generateReportData(sortedPages);
+    downloadReport(reportData);
 }
 
 function sortPages(pages) {
@@ -23,7 +29,46 @@ function sortPages(pages) {
     return pagesArr
 }
 
+function generateReportData(sortedPages) {
+    let reportData = "===========" + "\n";
+    reportData += "REPORT" + "\n";
+    reportData += "===========" + "\n";
+    
+    for (const sortedPage of sortedPages) {
+        const url = sortedPage[0];
+        const hits = sortedPage[1];
+        reportData += `Found ${hits} links to page: ${url}` + "\n";
+    }
+    
+    reportData += "===========" + "\n";
+    reportData += "END REPORT" + "\n";
+    reportData += "===========" + "\n";
+    
+    return reportData;
+}
+
+function downloadReport(reportData) {
+    fs.writeFile('website_report.txt', reportData, (err) => {
+        if (err) {
+            console.error("Error occurred while downloading the report:", err);
+        } else {
+            console.log("Report downloaded successfully.");
+            convertToExcel(reportData);
+        }
+    });
+}
+
+function convertToExcel(reportData) {
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet([[reportData]]);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Website Report");
+    const excelFilePath = 'website_report.xlsx';
+    XLSX.writeFile(workbook, excelFilePath);
+    console.log("Excel report created:", excelFilePath);
+}
+
 module.exports = {
     sortPages,
-    printReport
+    printReport,
+    downloadReport
 }
